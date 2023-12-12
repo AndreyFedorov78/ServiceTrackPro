e868d8ebd3addd00a3296baf436516a621109112
Andrey Fedorov <87073832+AndreyFedorov78@users.noreply.github.com>
2023-12-12 12:30:39 +0300

    первые шаблоны

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,72 @@
+
+
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+
+
+
+  <div>
+    <h1>-Моя страница-</h1>
+    <router-view></router-view>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* Глобальные стили для body */
+body {
+  /* Указание пути к вашему изображению в качестве фона */
+  background-image: url('@/assets/background1.png');
+
+  /* Подстройка изображения под ширину экрана и повторение по вертикали */
+  background-size: 100% auto; /* Подстроить по ширине, высота - авто */
+  background-repeat: repeat-y; /* Повторять только по вертикали */
+
 }
 </style>
+
+
+<script>
+export default {
+   delimiters: ["[[", "]]"] // Устанавливаем delimiters внутри компонента
+  // ...
+};
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,45 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+//import MyComponent from './MyComponent.vue'; // Импортируем компонент
+//import MyComponent2 from './MyComponent2.vue'; // Импортируем компонент
+import { createRouter, createWebHistory } from 'vue-router';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-createApp(App).mount('#app')
+
+
+
+const app = createApp(App);
+app.use(BootstrapVue)
+app.use(IconsPlugin)
+
+//import { ButtonGroupPlugin } from 'bootstrap-vue'
+//app.use(ButtonGroupPlugin)
+//import { NavbarPlugin } from 'bootstrap-vue'
+//app.use(NavbarPlugin)
+
+
+
+// Optionally install the BootstrapVue icon components plugin
+
+
+
+
+// Регистрируем компонент MyComponent глобально или внутри других компонентов
+//app.component('my-component', MyComponent);
+//app.component('my-component2', MyComponent2);
+
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+ //   { path: '/', component: MyComponent }, // Путь к первой странице
+ //   { path: '/second', component: MyComponent2 }, // Путь ко второй странице
+  ],
+});
+
+
+app.use(router); // Используем маршрутизатор
+
+app.mount('#app');
